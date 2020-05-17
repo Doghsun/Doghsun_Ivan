82471f057388d827543c66b4b788b1622c6265f0
dev <dev@mail.com>
2020-05-17 09:33:32 +0900

    Updates

---
+++ precache-manifest.35bcb642c08628daeb51ac593264d368.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738ca3c077f188e65f51ac629130bd71",
+    "url": "/Doghsun_Ivan/index.html"
+  },
+  {
+    "revision": "2d7ae04dd7c019896f93",
+    "url": "/Doghsun_Ivan/static/css/main.e80a825a.chunk.css"
+  },
+  {
+    "revision": "1dec4969b6f4ab4e35c3",
+    "url": "/Doghsun_Ivan/static/js/2.84a586ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Doghsun_Ivan/static/js/2.84a586ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7ae04dd7c019896f93",
+    "url": "/Doghsun_Ivan/static/js/main.9ed47bbb.chunk.js"
+  },
+  {
+    "revision": "8fa3ed886d1763574924",
+    "url": "/Doghsun_Ivan/static/js/runtime-main.bbc112d2.js"
+  },
+  {
+    "revision": "802631b9c700b48d636e2d806c8013aa",
+    "url": "/Doghsun_Ivan/static/media/Alekcandra_F.802631b9.png"
+  },
+  {
+    "revision": "f6449a2a7a52d1a4b292260152ea6b8a",
+    "url": "/Doghsun_Ivan/static/media/Cutar.f6449a2a.png"
+  },
+  {
+    "revision": "3155fa301e6c47fb642034d8ee7bdae7",
+    "url": "/Doghsun_Ivan/static/media/Evdok_Ctruchova.3155fa30.png"
+  },
+  {
+    "revision": "3991e6e2299d7f993e7da07914d0067d",
+    "url": "/Doghsun_Ivan/static/media/Ivan_F_Platonov.3991e6e2.png"
+  },
+  {
+    "revision": "df98fd2c9a460770c08babf00f7075b6",
+    "url": "/Doghsun_Ivan/static/media/N_MarkovI.df98fd2c.png"
+  },
+  {
+    "revision": "2abb00b47240dfcb62b48e637b8a1d5c",
+    "url": "/Doghsun_Ivan/static/media/Nikolay_F_Platonov.2abb00b4.png"
+  },
+  {
+    "revision": "64123ef0db027613b1359e0da156d646",
+    "url": "/Doghsun_Ivan/static/media/Platonovu_Dogsun.64123ef0.png"
+  },
+  {
+    "revision": "d8ae60dde0588a0a64b86eb7fa97f8fd",
+    "url": "/Doghsun_Ivan/static/media/Без.d8ae60dd.png"
+  }
+]);
